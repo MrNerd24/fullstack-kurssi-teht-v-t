94127d6686f28e4c9751995b5be9fbf4fbf84e08
MrNerd24 <juuso.lassila@helsinki.fi>
2018-01-16 10:50:13 +0200

    Tehtävä 1.1

--- Osa1/project1/src/index.js
+++ Osa1/project1/src/index.js
@@ -1,8 +1,60 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const Otsikko = (props) => {
+    return (
+        <h1>{props.kurssi}</h1>
+    )
+}
+
+const Osa = (props) => {
+    return(
+        <p>{props.osa} {props.tehtavia}</p>
+    )
+}
+
+const Sisalto = (props) => {
+    return(
+        props.osat.map((osa)=> {
+            return(
+                <Osa osa={osa.osa} tehtavia={osa.tehtavia}/>
+            )
+        })
+    )
+}
+
+const Yhteensa = (props) => {
+    return(
+        <p>yhteensä {props.summa} tehtävää</p>
+    )
+}
+
+const App = () => {
+    const osat = [
+        {
+            osa: 'Reactin perusteet',
+            tehtavia: 10
+        },
+        {
+            osa: 'Tiedonvälitys propseilla',
+            tehtavia: 7
+        },
+        {
+            osa: 'Komponenttien tila',
+            tehtavia: 14
+        }
+    ]
+
+    return (
+        <div>
+            <Otsikko kurssi='Half Stack -sovelluskehitys'/>
+            <Sisalto osat={osat}/>
+            <Yhteensa summa={osat.reduce((summa, osa) => {return summa + osa.tehtavia}, 0)}/>
+        </div>
+    )
+}
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
+)
