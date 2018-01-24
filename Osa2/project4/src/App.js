4a1d195eda9286e4a0211014db073709a2ac5fea
MrNerd24 <juuso.lassila@helsinki.fi>
2018-01-24 12:32:10 +0200

    Tehtävä 2.6

---
+++ Osa2/project4/src/App.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            persons: [
+                { name: 'Arto Hellas' }
+            ],
+            newName: ''
+        }
+    }
+
+    handleSubmit = (event) => {
+
+        event.preventDefault()
+        this.setState({
+            persons: [...this.state.persons, {name: this.state.newName}],
+            newName: ""
+        })
+    }
+
+    handleNameChange = (event) => {
+        let value = event.target.value
+        this.setState({
+            newName: value
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Puhelinluettelo</h2>
+                <form onSubmit={this.handleSubmit}>
+                    <div>
+                        nimi: <input value={this.state.newName} onChange={this.handleNameChange} />
+                    </div>
+                    <div>
+                        <button type="submit">lisää</button>
+                    </div>
+                </form>
+                <h2>Numerot</h2>
+                <ul>
+                    {this.state.persons.map((person) => {
+                        return(
+                            <li key={person.name}>{person.name}</li>
+                        )
+                    })}
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default App
