d6b4073e8487508e081bf483342e034a0f89066d
MrNerd24 <juuso.lassila@helsinki.fi>
2018-02-08 13:28:28 +0200

    Tehtävä 4.5

--- Osa4/projekti6/utils/list_helpers.js
+++ Osa4/projekti6/utils/list_helpers.js
@@ -6,6 +6,34 @@ const totalLikes = (blogs) => {
 	return blogs.reduce((sum, val) => sum+val.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+	let index = maxIndex(blogs, (blog) => blog.likes);
+	if(index >= 0) {
+		return blogs[index]
+	} else {
+		return null;
+	}
+
+}
+
+const maxIndex = (list, value) => {
+	if(list.length === 0) {
+		return -1;
+	}
+	let maxValue = value(list[0])
+	let maxIndex = 0
+
+	list.forEach((item, index) => {
+		let itemValue = value(item)
+		if(itemValue > maxValue) {
+			maxValue = itemValue
+			maxIndex = index
+		}
+	})
+
+	return maxIndex
+}
+
 module.exports = {
-	dummy, totalLikes
+	dummy, totalLikes, favoriteBlog
 }
