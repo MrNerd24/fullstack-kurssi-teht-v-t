3e6ee1fcfbbfb6f31a9b2e13ab6ba8ea7f78d480
MrNerd24 <juuso.lassila@helsinki.fi>
2018-02-19 09:10:42 +0200

    tehtävä 5.14

--- Osa4/projekti6/bloglist-frontend/src/tests/components/Blog.test.js
+++ Osa4/projekti6/bloglist-frontend/src/tests/components/Blog.test.js
@@ -5,6 +5,45 @@ import Blog from '../../components/Blog'
 
 describe("<Blog />", () => {
 
-	it("")
+	let blog = null;
+	let likeFunc = jest.fn()
+	let deleteFunc = jest.fn()
+
+	beforeEach(() => {
+		blog = shallow(
+			<Blog
+				blog={{
+					title: "Hieno blogi",
+					author: "Minä",
+					url: "ei ole",
+					likes: 50,
+					user: {
+						username: "Minä",
+						name: "Minä"
+					}
+				}}
+				user = {{
+					username: "Minä",
+					name: "Minä"
+				}}
+				onBlogLike={likeFunc}
+				onBlogDelete={deleteFunc}
+			/>
+		)
+	})
+
+	it("Shows only name and author, and after clicking, more info", () => {
+		expect(blog.html()).toEqual(expect.stringContaining("Hieno blogi"))
+		expect(blog.html()).toEqual(expect.stringContaining("Minä"))
+		expect(blog.html()).not.toEqual(expect.stringContaining("ei ole"))
+		expect(blog.html()).not.toEqual(expect.stringContaining("50"))
+
+		blog.first().simulate("click")
+
+		expect(blog.html()).toEqual(expect.stringContaining("Hieno blogi"))
+		expect(blog.html()).toEqual(expect.stringContaining("Minä"))
+		expect(blog.html()).toEqual(expect.stringContaining("ei ole"))
+		expect(blog.html()).toEqual(expect.stringContaining("50"))
+	})
 
 })
